f72972f967bfea65ebe5278ba87218f598ddcf5d
chaosRobe <dominik.panasz@outlook.com>
2024-06-15 18:27:04 +0200

    Zad_1

---
+++ kolokwium_przykladowe/kolokwium_przykladowe/main.cpp
@@ -0,0 +1,17 @@
+//#include 
+class Klient
+{
+	char nazwa[25];
+};
+class Konto_Bankowe 
+{
+	int numer_konta[26];
+	double stan_konta;
+	bool aktywne; //hmm
+	Klient* wlasciciel;
+};
+
+int main() 
+{
+
+}
