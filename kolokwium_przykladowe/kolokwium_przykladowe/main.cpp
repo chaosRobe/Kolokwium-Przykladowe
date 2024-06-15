52a93cd6e6fdf4b0dccc59f5b7e9226370cb44dd
chaosRobe <dominik.panasz@outlook.com>
2024-06-15 20:21:30 +0200

    Kolokwium - settery i gettery

--- kolokwium_przykladowe/kolokwium_przykladowe/main.cpp
+++ kolokwium_przykladowe/kolokwium_przykladowe/main.cpp
@@ -1,14 +1,74 @@
 //#include 
 class Klient
 {
+protected:
 	char nazwa[25];
 };
 class Konto_Bankowe 
 {
-	int numer_konta[26];
+protected:
+	unsigned int numer_konta[26];
 	double stan_konta;
-	bool aktywne; //hmm
+	bool stan_aktywnosci; //hmm
 	Klient* wlasciciel;
+public:
+	Konto_Bankowe()
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			numer_konta[i] = 0;
+		}
+		stan_konta = 0;
+		stan_aktywnosci = false;
+		wlasciciel = nullptr;
+	}
+	void set_numer_konta(unsigned int id[26])
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			if(id[i]>9) numer_konta[i] = 9;
+			else numer_konta[i] = id[i];
+		}
+	}
+	Konto_Bankowe(unsigned int id[26], double $, bool stan, Klient* klient)
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			if (id[i] > 9) numer_konta[i] = 9;
+			else numer_konta[i] = id[i];
+		}
+		stan_konta = $;
+		stan_aktywnosci = stan;
+		wlasciciel = klient;
+	}
+	void set_stan_konta(double $)
+	{
+		stan_konta = $;
+	}
+	void set_czy_aktywne(bool taknie)
+	{
+		stan_aktywnosci = taknie;
+	}
+	void set_wlasciciel(Klient* klient)
+	{
+		wlasciciel = klient;
+	}
+	unsigned int* get_numer_konta() 
+	{
+		return numer_konta;
+	}
+	double get_stan_konta()
+	{
+		return stan_konta;
+	}
+	bool get_stan_aktywnosci()
+	{
+		return stan_aktywnosci;
+	}
+	Klient* get_wlasciciel()
+	{
+		return wlasciciel;
+	}
 };
 
 int main() 
